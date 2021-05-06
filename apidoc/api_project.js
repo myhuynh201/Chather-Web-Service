e18989830e008673b59a14bf615953a1970dbe92
Alec Mac <alecmac95@gmail.com>
2021-05-06 13:02:09 -0700

    update apidoc base url

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -3,7 +3,7 @@ define({
   "version": "0.1.0",
   "description": "The Chather Web Service API Documentation",
   "title": "Lab API Doc",
-  "url": " https://tcss450-android-app.herokuapp.com/",
+  "url": " https://tcss450-android-app.herokuapp.com",
   "order": [
     "Register",
     "Sign In"
@@ -13,7 +13,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-06T19:38:25.725Z",
+    "time": "2021-05-06T20:01:46.628Z",
     "url": "https://apidocjs.com",
     "version": "0.27.1"
   }
