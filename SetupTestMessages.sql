52fcb1b236dabfd6da56a0dc9644e6e536607a89
Alec Mac <alecmac95@gmail.com>
2021-05-17 00:07:33 -0700

    added beginning chat endpoints

--- SetupTestMessages.sql
+++ SetupTestMessages.sql
@@ -13,9 +13,9 @@ WHERE Email='test1@test.com';
 
 --Add the User test1  (password is: test12345)
 INSERT INTO 
-    Members(FirstName, LastName, Username, Email, Password, Salt)
+    Members(FirstName, LastName, Username, Email, Password, Salt, Verification)
 VALUES
-    ('test1First', 'test1Last', 'test1', 'test1@test.com', 'aafc93bbad0671a0531fa95168c4691be3a0d5e033c33a7b8be9941d2702e566', '5a3d1d9d0bda1e4855576fe486c3a188e14a3f1a381ea938cacdb8c799a3205f');
+    ('test1First', 'test1Last', 'test1', 'test1@test.com', 'aafc93bbad0671a0531fa95168c4691be3a0d5e033c33a7b8be9941d2702e566', '5a3d1d9d0bda1e4855576fe486c3a188e14a3f1a381ea938cacdb8c799a3205f', 1);
 
 --Remove the user test2
 DELETE FROM Members 
@@ -23,9 +23,9 @@ WHERE Email='test2@test.com';
 
 --Add the User test2  (password is: test12345)
 INSERT INTO 
-    Members(FirstName, LastName, Username, Email, Password, Salt)
+    Members(FirstName, LastName, Username, Email, Password, Salt, Verification)
 VALUES
-    ('test2First', 'test2Last', 'test2', 'test2@test.com', 'aafc93bbad0671a0531fa95168c4691be3a0d5e033c33a7b8be9941d2702e566', '5a3d1d9d0bda1e4855576fe486c3a188e14a3f1a381ea938cacdb8c799a3205f');
+    ('test2First', 'test2Last', 'test2', 'test2@test.com', 'aafc93bbad0671a0531fa95168c4691be3a0d5e033c33a7b8be9941d2702e566', '5a3d1d9d0bda1e4855576fe486c3a188e14a3f1a381ea938cacdb8c799a3205f', 1);
 
 --Remove the user test3
 DELETE FROM Members 
@@ -33,9 +33,9 @@ WHERE Email='test3@test.com';
 
 --Add the User test3 (password is: test12345)
 INSERT INTO 
-    Members(FirstName, LastName, Username, Email, Password, Salt)
+    Members(FirstName, LastName, Username, Email, Password, Salt, Verification)
 VALUES
-    ('test3First', 'test3Last', 'test3', 'test3@test.com', 'aafc93bbad0671a0531fa95168c4691be3a0d5e033c33a7b8be9941d2702e566', '5a3d1d9d0bda1e4855576fe486c3a188e14a3f1a381ea938cacdb8c799a3205f');
+    ('test3First', 'test3Last', 'test3', 'test3@test.com', 'aafc93bbad0671a0531fa95168c4691be3a0d5e033c33a7b8be9941d2702e566', '5a3d1d9d0bda1e4855576fe486c3a188e14a3f1a381ea938cacdb8c799a3205f', 1);
 
 --Create Global Chat room, ChatId 1
 INSERT INTO
