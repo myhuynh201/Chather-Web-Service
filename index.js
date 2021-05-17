fbb5d289f63b49090f6321009b0e44683cecb606
Alex CO <alejandrocossio1@gmail.com>
2021-05-17 13:02:46 -0700

    Added weather endpoint

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ app.use("/doc", express.static('apidoc'))
 app.use("/auth", require('./routes/signin.js'))
 app.use("/auth", require('./routes/register.js'))
 app.use("/verify", require('./routes/verify.js'))
+//The app will navigate to the weather.js file once we include /weather in the url of our app
+//we apparently need to check the token in order to use the API, not the jwt though
+app.use("/weather", middleware.checkToken, require("./routes/weather.js"))
 
 const validation = require('./utilities').validation 
 let isStringProvided = validation.isStringProvided
---
+++ routes/weather.js
@@ -0,0 +1,69 @@
+const API_KEY = process.env.OPENWEATHERMAPS_DOT_ORG_KEY
+
+//express is the framework we're going to use to handle requests
+const express = require('express')
+
+//request module is needed to make a request to a web service
+const request = require('request')
+
+var router = express.Router()
+
+/**
+ * @api {get} /weather Request a JSONObject which should bring the 
+ * weather data for a desired location
+ * @apiName OneCallApi
+ * @apiGroup OpenWeatherMap
+ * 
+ * @apiHeader {String} authorization JWT provided from Auth get
+ * 
+ * @apiDescription This endpoint is a pass through to the OpenWeatherMap.org API. All parameters will
+ * pass on to the https://api.openweathermap.org/data/2.5/onecall? . 
+ * See the <a href="https://openweathermap.org/api/one-call-api">OpenWeatherMap.org documentation</a> 
+ * for additional parameters and additional information. An API key is required in order to
+ * make calls with this endpoint. 
+ */ 
+router.get("/weather", (req, res) => {
+
+    //An example of the call made using the One Call API.
+    //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    let url = `https://api.openweathermap.org/data/2.5/onecall?&appid={API_KEY}`
+    
+    //find the query string (parameters) sent to this end point and pass them on to
+    // OpenWeatherMap api call 
+    let n = req.originalUrl.indexOf('?') + 1
+    if(n > 0) {
+        url += '&' + req.originalUrl.substring(n)
+    }
+
+    //Retrieve data from query params
+    const lon = request.body.lon
+    const lat = request.body.lat
+    const exclude = request.body.exclude
+    const units = request.body.units
+
+    //I guess I can hardcode the url. Needs checking for correctness and whether this string, while not a string literal, is fine.
+    let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=" + exclude + "&units=" + units +"&appid=" + API_KEY
+
+    /*What this code above is doing is that it is taking the URL from the WeatherViewModel and then extracting the 
+    parameters that were sent along for the ride. */
+
+    //When this web service gets a request, make a request to the OpenWeatherMap Web service
+    request(url, function (error, response, body) {
+        if (error) {
+            res.send(error)
+        } else {
+            // pass on everything (try out each of these in Postman to see the difference)
+            // res.send(response);
+            
+            // or just pass on the body
+
+            var n = body.indexOf("{")
+            var nakidBody = body.substring(n - 1)
+
+            res.send(nakidBody)
+        }
+    })
+
+})
+
+module.exports = router
